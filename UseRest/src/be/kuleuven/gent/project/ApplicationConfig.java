15388ce308d5374af9778961518c4303c97111b7
PelleReyniers <pelle.reyniers@protonmail.com>
2018-03-13 12:05:28 +0100

    Rest Update

---
+++ UseRest/src/be/kuleuven/gent/project/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package be.kuleuven.gent.project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.owlike.genson.ext.jaxrs.GensonJsonConverter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("rest_unicorn")
+public class ApplicationConfig extends Application {
+
+	private final Set<Class<?>> classes;
+	
+	final ResourceConfig config = new ResourceConfig().register(GensonJsonConverter.class);
+
+	public ApplicationConfig() {
+		HashSet<Class<?>> c = new HashSet<>();
+		c.add(BasicRestService.class);
+		c.add(ParameterRestService.class);
+		c.add(ContentTypesRestService.class);
+		c.add(ResponseRestService.class);
+
+		classes = Collections.unmodifiableSet(c);
+	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return classes;
+	}
+}
---
+++ UseRest/src/be/kuleuven/gent/project/BasicRestService.java
@@ -0,0 +1,32 @@
+package be.kuleuven.gent.project;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("basic_service")
+public class BasicRestService {
+	
+	@GET
+	public Boolean getBasicService()
+	{
+		System.out.println("get basic service");
+		return true;
+	}
+	
+	@POST
+	public Boolean postBasicService()
+	{
+		System.out.println("post basic service");
+		return true;
+	}
+	
+	@DELETE
+	public Boolean deleteBasicService()
+	{
+		System.out.println("delete basic service");
+		return true;
+	}
+}
