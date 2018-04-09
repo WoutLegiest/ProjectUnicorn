7469248c2f04d8f85d1f84c14c294294b82d5c1e
Boris Wauters <boris.wauters@telenet.be>
2018-04-09 17:30:19 +0200

    changed method name "verwerkData" to "processData"

--- UseEJBClient/src/be/kuleuven/gent/project/ejb/ApplicationManagementEJBLocal.java
+++ UseEJBClient/src/be/kuleuven/gent/project/ejb/ApplicationManagementEJBLocal.java
@@ -1,7 +1,5 @@
 package be.kuleuven.gent.project.ejb;
 
-import com.mathworks.engine.EngineException;
-
 import javax.ejb.Local;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,5 +8,5 @@ import java.util.concurrent.ExecutionException;
 @Local
 public interface ApplicationManagementEJBLocal {
 
-    public LinkedList<LinkedList<Double>> verwerkData(LinkedList<LinkedList<Double>> input ) throws InterruptedException, ExecutionException;
+    public LinkedList<LinkedList<Double>> processData(List<LinkedList<Double>> input ) throws InterruptedException, ExecutionException;
 }
