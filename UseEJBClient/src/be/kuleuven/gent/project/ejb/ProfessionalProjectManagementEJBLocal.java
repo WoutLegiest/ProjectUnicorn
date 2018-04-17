635501c66a3a7c1b56a812e69686c9ad75902324
Wouter Legiest <wouter.legiest@me.com>
2018-04-17 11:52:31 +0200

    Verdere aanpassingen EJB

---
+++ UseEJBClient/src/be/kuleuven/gent/project/ejb/ProfessionalProjectManagementEJBLocal.java
@@ -0,0 +1,22 @@
+package be.kuleuven.gent.project.ejb;
+
+import be.kuleuven.gent.project.data.ProfessionalMeasurement;
+import be.kuleuven.gent.project.data.ProfessionalProject;
+
+import javax.ejb.Local;
+import java.util.List;
+
+@Local
+public interface ProfessionalProjectManagementEJBLocal {
+
+    List<ProfessionalProject> findAllProjects();
+
+    ProfessionalProject findProject(long id);
+
+    List<ProfessionalMeasurement> findAllMeasurements(Long projectId);
+
+    List<String> projectByLocations(float latitude, float longitude);
+
+    ProfessionalProject makeProject(String name, String location, float latitude, float longitude, String desc);
+
+}
