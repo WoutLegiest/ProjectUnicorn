750cb62a4ef9f33566aa08e4993bed9114b38615
Wouter Legiest <wouter.legiest@me.com>
2018-05-14 23:48:34 +0200

    Laatste fout uit STEM

--- UseWeb/src/be/kuleuven/gent/project/jsf/controller/StemProjectController.java
+++ UseWeb/src/be/kuleuven/gent/project/jsf/controller/StemProjectController.java
@@ -68,7 +68,7 @@ public class StemProjectController implements Serializable {
     public String newProject(){
         project.setTeacher(findTeacher());
         stemProjectEJB.createProject(project, numberOfGroups);
-        return "projecten?faces-redirect=true";
+        return "projects?faces-redirect=true";
     }
 
     /**
